b7bea2bbb2858c0decd5015fa6a1283f9365abf5
Ian G <ian.glazer@gmail.com>
2013-12-24 11:51:58 -0600

    upload form created

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@
 
 exports.index = function(req, res){
   res.render('index', { title: 'Imagi - Ian\'s Image Uploader and such',
-  						message: 'Well don\'t just stand there!' });
+  						message: 'Well don\'t just stand there!',
+  						links: {
+  							uploader: { url:'/uploader', text: 'Upload an image'},
+  							catalog: { url:'/catalog', text:'See all the images'}
+  						}	
+  					 });
 };
